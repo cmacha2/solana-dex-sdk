43e987fda650f819184bcc08eeb34c80f9f2cf00
cmacha2 <cmacha2.08@gmail.com>
2025-02-02 17:12:15 -0500

    Update to handle modules and commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-
+// Exportaciones principales
 export { SolanaDexClient } from './SolanaDexClient';
 
 export { SwapConfig } from './types';
 export { TokenPrice } from './types';
 
 export * from './utils';
+
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = {
+    SolanaDexClient: require('./SolanaDexClient').SolanaDexClient,
+    SwapConfig: require('./types').SwapConfig,
+    TokenPrice: require('./types').TokenPrice,
+    ...require('./utils'),  
+  };
+}
